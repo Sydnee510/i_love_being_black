b067d01700facf2e76db0baed83cce8e4955858a
sydnee510 <sbsydnee161416@gmail.com>
2020-06-10 16:19:06 -0700

    deleted user table

--- love_black_backend/db/schema.rb
+++ love_black_backend/db/schema.rb
@@ -10,19 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_213524) do
+ActiveRecord::Schema.define(version: 2020_06_10_200638) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.string "state"
     t.string "country"
     t.integer "likes"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
