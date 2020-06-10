6ea143dc7306c630895347219aa8b4b882c6d354
sydnee510 <sbsydnee161416@gmail.com>
2020-06-10 13:30:12 -0700

    build posts table/migrated

--- love_black_backend/db/migrate/20200610200638_create_posts.rb
+++ love_black_backend/db/migrate/20200610200638_create_posts.rb
@@ -1,8 +1,12 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-
-      t.timestamps
+      t.text :content
+      t.string :state
+      t.string :country
+      t.integer :likes
+      t.integer :user_id
+      t.timestamps null: false
     end
   end
 end
