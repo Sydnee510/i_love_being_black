572d6d9860aff21fc20bcebfb036b8a471b59e8e
sydnee510 <sbsydnee161416@gmail.com>
2020-06-10 18:43:40 -0700

    fetch posts from db to js console

--- love_black_frontend/src/index.js
+++ love_black_frontend/src/index.js
@@ -1,6 +1,6 @@
 console.log("testing...")
 // test that we can get data from the backend
 const BACKEND_URL = 'http://localhost:3000';
-fetch(`${BACKEND_URL}/test`)
+fetch(`${BACKEND_URL}/posts`)
   .then(response => response.json())
   .then(parsedResponse => console.log(parsedResponse));
