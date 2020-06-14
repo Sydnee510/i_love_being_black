4bfd2532f747ec5e277b22eed15d843e1833c759
sydnee510 <sbsydnee161416@gmail.com>
2020-06-13 21:26:15 -0700

    kick off app with creating new app

--- love_black_frontend/src/index.js
+++ love_black_frontend/src/index.js
@@ -1,6 +1,1 @@
-console.log("testing...")
-// test that we can get data from the backend
-const BACKEND_URL = 'http://localhost:3000';
-fetch(`${BACKEND_URL}/posts`)
-  .then(response => response.json())
-  .then(parsedResponse => console.log(parsedResponse));
+const app = new App()
