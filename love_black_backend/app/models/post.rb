9f211be5658410dd9dc65f8b47b2b72ea0f95725
sydnee510 <sbsydnee161416@gmail.com>
2020-06-29 16:21:26 -0700

    validates state to start with a capitol letter

--- love_black_backend/app/models/post.rb
+++ love_black_backend/app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
     validates :content, presence: true
     validates :state, presence: true, length: { minimum: 4 }
+    validates_format_of :state, :with => /[A-Z]/
     validates :country, presence: true, length: { is: 2 }
     validates_format_of :country, :with => /[A-Z]/
 end
