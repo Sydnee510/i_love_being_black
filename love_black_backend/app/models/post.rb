73c5dfb667875ec44ade5d73f849299ae4454746
sydnee510 <sbsydnee161416@gmail.com>
2020-06-29 16:09:19 -0700

    validates country with 2 capitol letters

--- love_black_backend/app/models/post.rb
+++ love_black_backend/app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
     validates :content, presence: true
     validates :state, presence: true, length: { minimum: 4 }
     validates :country, presence: true, length: { is: 2 }
+    validates_format_of :country, :with => /[A-Z]/
 end
