8d7a2083d6001f89706bc4a52c54c79008112315
sydnee510 <sbsydnee161416@gmail.com>
2020-07-03 20:17:25 -0700

    add serialzers to posts controller

--- love_black_backend/app/controllers/posts_controller.rb
+++ love_black_backend/app/controllers/posts_controller.rb
@@ -1,32 +1,43 @@
 class PostsController < ApplicationController
     def index
-        @posts = Post.all 
-        render json: @posts, status: 200
+        posts = Post.all 
+        options = {
+            include: [:comments]
+        }
+        render json: PostSerializer.new(posts, options)
+        # render json: posts, status: 200
     end
     def show 
-        @post = Post.find(params[:id])
-        render json: @post, status: 200
+        post = Post.find(params[:id])
+        options = {
+            include: [:comments]
+        }
+        render json: PostSerializer.new(post, options)
+        #render json: @post, status: 200
     end 
     def create 
-        @post = Post.new(post_params)
-        if @post.save
-        render json: @post, status: 200
+        post = Post.new(post_params)
+        if post.save
+        render json: PostSerializer.new(post)    
+        #render json: @post, status: 200
         else 
-            render :json => { :errors => @post.errors.full_messages }, :status => 422
+            render :json => { :errors => post.errors.full_messages }, :status => 422
         end
     end
     def update 
-        @post = Post.find(params[:id])
-        if @post.update(post_params)
-        render json: @post, status: 200
+        post = Post.find(params[:id])
+        if post.update(post_params)
+        render json: PostSerializer.new(post)
+        #render json: @post, status: 200
         else 
-            render :json => { :errors => @post.errors.full_messages }, :status => 422
+            render :json => { :errors => post.errors.full_messages }, :status => 422
         end
     end
     def destroy
-        @post = Post.find(params[:id])
-        @post.delete
-        render json: {postId: @post.id}
+        post = Post.find(params[:id])
+        post.delete
+        render json: PostSerializer.new(post)
+        #render json: {postId: post.id}
     end 
     private
     def post_params
--- love_black_backend/app/serializers/post_serializer.rb
+++ love_black_backend/app/serializers/post_serializer.rb
@@ -1,4 +1,5 @@
 class PostSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :content, :state, :country, :likes
+  attributes :id, :content, :state, :country, :likes
+  has_many :comments
 end
