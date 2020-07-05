4a2fdc6a2c3e1ad98aad325e62375717e36d328f
sydnee510 <sbsydnee161416@gmail.com>
2020-07-05 00:24:45 -0700

    add attributes to comment.js

--- love_black_frontend/src/components/comment.js
+++ love_black_frontend/src/components/comment.js
@@ -2,6 +2,8 @@ class Comment {
     constructor(attributes){
         this.id = attributes.id
         this.text = attributes.text
-        
+        this.post_id = attributes.post_id
+        this.commentsAdapter = new CommentsAdapter()
     }
+
 }
