fa0cfdd364c6f804854e21657948428278e001d0
sydnee510 <sbsydnee161416@gmail.com>
2020-07-05 00:22:11 -0700

    post wont render w/ PostSerializer

--- love_black_backend/app/controllers/posts_controller.rb
+++ love_black_backend/app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
     def index
-        posts = Post.all 
+        @posts = Post.all 
         # options = {
         #     include: [:comments]
         # }
-        render json: PostSerializer.new(posts)
-         #render json: @posts, status: 200
+        #render json: PostSerializer.new(posts)
+         render json: @posts, status: 200
     end
     def show 
         post = Post.find(params[:id])
