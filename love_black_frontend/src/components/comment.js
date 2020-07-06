de0a7d0370aec6e597587db09a1b30887e1fc0dd
sydnee510 <sbsydnee161416@gmail.com>
2020-07-06 10:23:43 -0700

    add bindings & event listeners to comments.js

--- love_black_frontend/src/components/comment.js
+++ love_black_frontend/src/components/comment.js
@@ -5,11 +5,16 @@ class Comment {
         this.id = id
         this.post_id = post_id
         this.commentsAdapter = new CommentsAdapter()
-        this.commentsContainer = document.getElementById('comments-container')
+        this.bindingsAndEventListeners()
+   
     }
-    // this.commentForm = document.querySelector("#new-comment-form")
-    // this.newCommentText = document.getElementById("new-comment-text")
-     //this.commentsContainer = document.getElementById('comments-container')
+    bindingsAndEventListeners(){
+        this.commentsContainer = document.getElementById('comments-container')
+        this.commentForm = document.getElementById('new-comment-form')
+        this.commentForm = addEventListener('submit', this.createComment.bind(this))
+        this.newCommentText = document.getElementById('new-comment-text')
+    }
+  
     createComment(e){
         e.preventDefault()
         const text = this.newCommentText.value
@@ -20,6 +25,7 @@ class Comment {
             //alert("post created!")
         })
     }
+
     render(){
         this.commentsContainer.innerHTML = this.comments.map(comment => comment.renderLi()).join('')
     }
