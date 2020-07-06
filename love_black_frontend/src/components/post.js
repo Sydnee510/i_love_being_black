c9876adf395dde0c4e11f9170a1962d47a4016db
sydnee510 <sbsydnee161416@gmail.com>
2020-07-06 09:52:47 -0700

    removed facebook like button

--- love_black_frontend/src/components/post.js
+++ love_black_frontend/src/components/post.js
@@ -8,7 +8,7 @@ class Post {
         this.comments = comments
     }
     renderLi(){
-        return `<li data-id=${this.id}>${this.content}, ${this.state}, ${this.country}, comments: ${this.comments},  <button type="button" id=${this.id} class="delete-post"> DELETE </button> <div class="fb-like" data-href="http://localhost:3000/posts/${this.id}" data-width="" allign-text="right" data-layout="box_count" data-action="like" data-size="small" data-share="false"></div>
+        return `<li data-id=${this.id}>${this.content}, ${this.state}, ${this.country}, comments: ${this.comments},  <button type="button" id=${this.id} class="delete-post"> DELETE </button>
          <form id = "new-comment-form">
 <input type = "text" id="new-comment-text" font-size: "medium;" placeholder= "comment:">
 <input type = "submit" value="Comment">
