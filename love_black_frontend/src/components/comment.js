f647d57010082d54eae08ffab210ace4a52f26d4
sydnee510 <sbsydnee161416@gmail.com>
2020-07-05 22:18:32 -0700

    trying to connect post.js with comment.js

--- love_black_frontend/src/components/comment.js
+++ love_black_frontend/src/components/comment.js
@@ -1,13 +1,15 @@
 class Comment {
     constructor(text, id, post_id){
+        this.comments = []
         this.text = text
         this.id = id
         this.post_id = post_id
         this.commentsAdapter = new CommentsAdapter()
+        this.commentsContainer = document.getElementById('comments-container')
     }
     // this.commentForm = document.querySelector("#new-comment-form")
     // this.newCommentText = document.getElementById("new-comment-text")
-    // this.commentsContainer = document.getElementById('comments-container')
+     //this.commentsContainer = document.getElementById('comments-container')
     createComment(e){
         e.preventDefault()
         const text = this.newCommentText.value
@@ -18,8 +20,8 @@ class Comment {
             //alert("post created!")
         })
     }
-    // render(){
-    //     this.commentsContainer.innerHTML = this.comments.map(comment => comment.renderLi()).join('')
-    // }
+    render(){
+        this.commentsContainer.innerHTML = this.comments.map(comment => comment.renderLi()).join('')
+    }
 
 }
--- love_black_frontend/src/components/post.js
+++ love_black_frontend/src/components/post.js
@@ -1,22 +1,19 @@
 class Post {
-    constructor(postJSON){
-        this.id = postJSON.id
-        this.content = postJSON.content
-        this.state = postJSON.state 
-        this.country = postJSON.country 
-        this.likes = postJSON.likes
-        this.data = postJSON.data
-        //this.form = document.querySelector("#new-comment-form")
-        this.comment = postJSON.comment
+    constructor(id, content, state, country, likes, comments){
+        this.id = id
+        this.content = content
+        this.state = state 
+        this.country = country 
+        this.likes = likes
+        this.comments = comments
     }
     renderLi(){
-        return `<li data-id=${this.id}>${this.content}, ${this.state}, ${this.country}, comments: ${this.comment},  <button type="button" id=${this.id} class="delete-post"> DELETE </button> <div class="fb-like" data-href="http://localhost:3000/posts/${this.id}" data-width="" allign-text="right" data-layout="box_count" data-action="like" data-size="small" data-share="false"></div> 
-        <form id = "new-comment-form">
-        <input type = "text" id="new-comment-text" font-size: "medium;" placeholder= "comment:">
-        <input type = "submit" value="Comment">
-        </form></li> `
+        return `<li data-id=${this.id}>${this.content}, ${this.state}, ${this.country}, comments: ${this.comments},  <button type="button" id=${this.id} class="delete-post"> DELETE </button> <div class="fb-like" data-href="http://localhost:3000/posts/${this.id}" data-width="" allign-text="right" data-layout="box_count" data-action="like" data-size="small" data-share="false"></div></li> `
     }
 }
-
+{/* <form id = "new-comment-form">
+<input type = "text" id="new-comment-text" font-size: "medium;" placeholder= "comment:">
+<input type = "submit" value="Comment">
+</form> */}
 
 
